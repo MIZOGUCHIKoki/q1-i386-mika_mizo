4ac4f1a44d6a4a7e0253dffb2a1744aa1bae7045
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2022-10-28 15:49:42 +0900

    演習1.8-3

---
+++ chap8/test_memsqsum.s
@@ -0,0 +1,55 @@
+	section .text
+	global _start
+	extern memsqsum
+	extern print_eax
+
+
+_start:
+	mov ebx, data1
+	mov ecx, ndata1
+	mov edx, 55
+	call memsqsum
+	call print_eax
+	cmp eax, edx
+	je next
+	mov ebx, 1
+	jmp end
+
+next:
+	mov ebx, data2
+	mov ecx, ndata2
+	mov edx, 0
+	call memsqsum
+	call print_eax
+	cmp eax, edx
+	je next2
+	mov ebx, 1
+	jmp end
+
+next2:
+	mov ebx, data3
+	mov ecx, ndata3
+	mov edx, 4294967295
+	call memsqsum
+	call print_eax
+	cmp eax, edx
+	je if
+	mov ebx, 1
+	jmp end
+
+if:	mov ebx, 0
+	jmp end
+	
+end:	
+	mov eax, 1
+	int 0x80
+
+
+
+
+data1:	dd 1, 2, 3, 4, 5
+ndata1:	equ ($ - data1) / 4
+data2:	dd 0, 0, 0
+ndata2:	equ ($ - data2) / 4
+data3:	dd 1, 1, 2, 4, 8, 13
+ndata3:	equ ($ - data3) / 4
