5637b2021b9274a1c749ba9fae4eebac7d89b682
MIKAMI Shu <250372y@ugs.kochi-tech.ac.jp>
2022-11-01 15:21:50 +0900

    print all data

--- chap9/test_sort.s
+++ chap9/test_sort.s
@@ -1,17 +1,22 @@
   section .text
   global  _start
-  extern  sort, print_eax
+  extern  sort2, print_eax
 
 _start:
   mov ebx,  data1
   mov ecx,  ndata1
-  call  sort
-
-  mov eax,  [data1]
+  call  sort2
+  mov edi,  0
+loop:
+  cmp edi,  ndata1 - 1
+  je  endp
+  mov eax,  [data1 + edi*4]
   call print_eax
-  mov eax,  [data1 + 4 * (ndata1 - 1)]
-  call  print_eax
+  inc edi
+  jmp loop
 
+
+endp:
   mov eax,  1
   mov ebx,  0
   int 0x80
