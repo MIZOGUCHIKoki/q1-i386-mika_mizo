e8372bfa735d82d09309eccf017be723ec42f03b
MIZOGUCHI Koki <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2022-11-01 14:08:31 +0900

    added bublesort

--- chap9/sort.java
+++ chap9/sort.java
@@ -26,6 +26,9 @@ public class sort {
         }
         printArray(sortdata);
 
+        buble(data1);
+        printArray(data1);
+
     }
 
     private static void push_heap(int[] data, int x) {
@@ -84,5 +87,22 @@ public class sort {
             }
         }
         return max;
+    }
+
+    private static void buble(int[] data) {
+        int max_index = 0;
+        for (int i = ndata - 1; i > 0; i--) {
+            max = data[0];
+            max_index = -0;
+            for (int j = 1; j <= i; j++) {
+                if (data[j] >= max) {
+                    max = data[j];
+                    max_index = j;
+                }
+            }
+            int m = data[max_index];
+            data[max_index] = data[i];
+            data[i] = m;
+        }
     }
 }
