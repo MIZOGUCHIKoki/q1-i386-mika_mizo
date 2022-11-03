b99fc7bfa830a9c27b27243e87ef6bde7f6bb518
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-11-03 03:32:58 +0000

    formatting

--- chap9/sort2.s
+++ chap9/sort2.s
@@ -1,7 +1,5 @@
   section .text
   global  sort2
-
-	
 sort2:
 	push esi
 	push edi
@@ -11,24 +9,24 @@ sort2:
 	push ebx
 	
 push_heap:
-	add [size], dword 1	;size++
-	mov eax, [size]		;int k = size
-	mov edi, [ebx + eax*4]	;data[size] = x
+	add [size], dword 1	        ;size++
+	mov eax, [size]		          ;int k = size
+	mov edi, [ebx + eax*4]	    ;data[size] = x
 	mov esi, 2
-	cmp ecx, 1		;データの数繰り返し
+	cmp ecx, 1		              ;データの数繰り返し
 	je end
 	dec ecx
 
 while:
-	cmp eax, 1		;k > 1
+	cmp eax, 1		              ;k > 1
 	jle push_heap
 	mov edx, 0
-	div esi			;k = k/2
-	cmp edi, [ebx + eax*4]	;data[k] > data[k/2]
+	div esi			                ;k = k/2
+	cmp edi, [ebx + eax*4]	    ;data[k] > data[k/2]
 	jle push_heap
-	mov edx, edi		;int i = data[l]
-	mov edi, [ebx + eax*4]	;data[k] = data[k/2]
-	mov [ebx + eax*4], edx	;data[k/2] = i
+	mov edx, edi		            ;int i = data[l]
+	mov edi, [ebx + eax*4]	    ;data[k] = data[k/2]
+	mov [ebx + eax*4], edx	    ;data[k/2] = i
 	jmp while
 	
 end:	
@@ -39,5 +37,6 @@ end:
 	pop edi
 	pop esi
 	ret
+
 	section .data
 size:	dd 0
