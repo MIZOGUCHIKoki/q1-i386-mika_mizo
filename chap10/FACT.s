1790b376bb856e7f9d1df84903206185d8df8d53
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2022-11-18 17:06:28 +0900

    jnz → jl

--- chap10/FACT.s
+++ chap10/FACT.s
@@ -1,14 +1,13 @@
   section .text
   global  FACT
   extern  MULT
-  extern  print_eax
  
 FACT:   ; 階乗F(ebx)
   push  ebx
   push  edx
   mov   eax,  1
   sub   ebx,  1
-  jnz   NEXT2
+  jg   NEXT2
   jmp   FIN2
 NEXT2:
   mov   edx,  1
@@ -20,7 +19,6 @@ LP2:
 FIN2:
   mov   esi,  eax
   mov   eax,  edx
-  call  print_eax
   mov   eax,  esi
   pop   edx
   pop   ebx
