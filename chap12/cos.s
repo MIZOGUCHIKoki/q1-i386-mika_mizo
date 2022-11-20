3984da135da84dce6d29c0b2cbea5e9e475d0f4f
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-11-20 06:38:41 +0000

    The calculation of division has not been completed. Able to preform addition and factorial calculations.

--- chap12/cos.s
+++ chap12/cos.s
@@ -1,24 +1,41 @@
 	section .text
 	global cos
+  extern  print_eax_int
 
 cos:
-	push esi
-	push edi
-	push edx
-	push ecx
-	push ebx
+	push  esi
+	push  edi
+	push  edx
+	push  ecx
+	push  ebx
+  
+  mov eax,  2
+  mov edx,  0
+  mul eax   ; in * in = edx eax
+  mov ebx,  eax ; ebx = in ^ 2
+  mul eax       ; (in ^ 2) ^ 2 = edx eax
+  mov ecx,  eax ; ecx = in ^ 4
+  mul ebx       ; in ^ 2 * in ^ 4 = edx eax
+  mov edi,  eax ; edi = in ^ 6 
+  
 
-	mov esi, 2
-	mov ecx, esi
-	mov edi, eax
+  mov eax,  1
+  shr ebx,  1   ; ebx / 2
+  sub eax,  ebx
 
-loop0:
-	mov edx, 0
-	cmp ecx, 0
-	je next
-	mul edi
-	dec ecx
-	jmp loop0
+  ;; ecx / 24
+  mov ebx,  ecx
+  shr ecx,  3   ; ecx / 8
+  add eax,  ecx ; eax += ecx
 
-next:
-	
+  ;; edx / 720
+  sub eax,  edi ; eax += edi
+
+  call  print_eax_frac
+endp:
+  pop ebx
+  pop ecx
+  pop edi
+  pop edi
+  pop esi
+  ret
--- chap12/test.s
+++ chap12/test.s
@@ -1,14 +1,10 @@
   section .text
   global  _start
-  extern  print_eax_frac
+  extern  cos
 
 _start:
-  mov eax,  0x12a00000
-  call print_eax_frac
+  mov eax, 2
+  call cos
   mov eax,  1
   mov ebx,  0
   int 0x80
-
-  section .data
-data1:  dd  -5,5,-10,2,10
-ndata:  equ ($ - data1) / 4
