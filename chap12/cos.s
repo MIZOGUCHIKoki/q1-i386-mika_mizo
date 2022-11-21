6131074b9b9b60468e3fa0539bcf8a2e266ea7a7
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-11-21 12:52:41 +0000

    Nagative numbers are supported

--- chap12/cos.s
+++ chap12/cos.s
@@ -10,16 +10,21 @@ cos:
 	push  ebx
 
   mov esi,  0x01000000
+  cmp eax,  0
+  jge pr
+  not eax
+  inc eax
   
+pr:
   ; x^2/2!
-  imul eax      ; in * in = edx eax
+  mul eax      ; in * in = edx eax
   mov ebx,  0x02000000  ; 除数 8.24
   div ebx      ; edx eax / ebx = eax
   sub esi,  eax
   push  eax
   
   ; x^4/4!
-  imul eax     ; (in ^ 2 / 2!) ^ 2
+  mul eax     ; (in ^ 2 / 2!) ^ 2
   mov ebx,  0x06000000  ; in ^ 4 / 4! 
   div ebx     ; edx eax / ebx = eax
   add esi,  eax
@@ -28,7 +33,7 @@ cos:
   mov ecx,  eax ; ecx = in ^ 4 / 4! 
   pop eax       ; eax = in ^ 2 / 2!
 
-  imul ecx       ; eax * ecx = edx eax
+  mul ecx       ; eax * ecx = edx eax
   mov edi,  0x0f000000 ; edx = 15
   div edi       ; edx eax / edi = eax
   sub esi,  eax
