c98b8d312e37ba93e2491a2edac9c67cdc3aa089
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-11-21 14:22:28 +0000

    suported over |pi/4|

--- chap12/cos.s
+++ chap12/cos.s
@@ -8,7 +8,7 @@ cos:
 	push  ecx
 	push  ebx
 
-  ;abs|eax|
+  ;abs|in|
   mov esi,  0x01000000
   cmp eax,  0
   jge pr
@@ -16,6 +16,18 @@ cos:
   inc eax
   
 pr:
+  ;; |in| > pi/2 ? 
+  mov ebx,  half_pi
+  mov ecx,  pi
+  cmp eax,  ebx
+  jle pr1
+  sub ecx,  eax   ; pi - x
+  mov eax,  ecx   ; eax = pi - x :: in'
+pr1:
+  ;; in' / 2
+  shr eax,  2   ; eax / 2 :: in' / 2
+
+cos_eax:
   ; x^2/2!
   mul eax      ; in * in = edx eax
   mov ebx,  0x02000000  ; 
@@ -36,13 +48,24 @@ pr:
   mul ecx       ; eax * ecx = edx eax
   mov edi,  0x0f000000 ; edx = 15
   div edi       ; edx eax / edi = eax
-  sub esi,  eax
+  sub esi,  eax 
 
+  ;; 2 * cos(in'/2)
+  mov eax,  esi ; eax = cos(in' / 2)
+  mul eax       ; cos(in'/2) ^ 2
+  ;; edx eax (16.48 -> 8.24)    ; 2 * cos(in/2)
+  mov ebx,  0x00800000  ; 0.5 [8.24]
+  div ebx
+  mov ebx,  0xffffffff  ; -1 [8.24]
+  add eax,  ebx
 endp:
-  mov eax,  esi
   pop ebx
   pop ecx
   pop edx
   pop edi
   pop esi
   ret
+
+  section .data
+half_pi:	equ	0x01921fb5  ;pi/2
+pi:	equ	0x03243f6a        ;pi
--- chap12/cos2.s
+++ chap12/cos2.s
@@ -1,64 +1,48 @@
 	section .text
-	global cos2
+	global cos
 
-cos2:
+cos:
 	push  esi
 	push  edi
 	push  edx
 	push  ecx
 	push  ebx
+
+  ;abs|eax|
+  mov esi,  0x01000000
+  cmp eax,  0
+  jge pr
+  not eax
+  inc eax
   
-	
-	mov edx, 0
-	mul eax			
-	mov ebx, eax
+pr:
+  ; x^2/2!
+  mul eax      ; in * in = edx eax
+  mov ebx,  0x02000000  ; 
+  div ebx      ; edx eax / ebx = eax
+  sub esi,  eax
+  push  eax
+  
+  ; x^4/4!
+  mul eax     ; (in ^ 2 / 2!) ^ 2
+  mov ebx,  0x06000000  ; in ^ 4 / 4! 
+  div ebx     ; edx eax / ebx = eax
+  add esi,  eax
+ 
+  ; x^6/6!
+  mov ecx,  eax ; ecx = in ^ 4 / 4! 
+  pop eax       ; eax = in ^ 2 / 2!
 
-	mov edx, 0
-	mul eax
-	mov ecx, eax
-
-	mov edx, 0
-	mul ebx
-	mov edi, eax
-	
-
-	mov esi, 2
-	mov eax, ebx
-	mov edx, eax
-	shl eax, 24
-	sar edx, 8
-	idiv esi
-	mov esi, 1
-	shl esi, 24
-	sub esi, eax
-	push esi
-
-	mov esi, 24
-	mov eax, ecx
-	mov edx, eax
-	shl eax, 24
-	sar edx, 8
-	idiv esi
-	pop esi
-	add esi, eax
-	push esi
-
-	mov esi, 720
-	mov eax, edi
-	mov edx, eax
-	shl eax, 24
-	sar edx, 8
-	idiv esi
-	pop esi
-	sub esi, eax
-	mov eax, esi
-	
+  mul ecx       ; eax * ecx = edx eax
+  mov edi,  0x0f000000 ; edx = 15
+  div edi       ; edx eax / edi = eax
+  sub esi,  eax
 
 endp:
-
+  mov eax,  esi
   pop ebx
   pop ecx
-  pop edi
+  pop edx
   pop edi
   pop esi
   ret
