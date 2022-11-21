93376bc971ed693fca39ed050d61139bb9d8d8f3
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-11-21 12:32:52 +0000

    executed sccesfully

--- chap12/cos.s
+++ chap12/cos.s
@@ -1,9 +1,6 @@
 	section .text
 	global cos
-  extern  print_eax_frac
-
-  ;; x^2 / 2! を返却する．
-  ;; 引数として，2を与えている（;in のところ）
+  extern print_eax_frac
 
 cos:
 	push  esi
@@ -11,16 +8,32 @@ cos:
 	push  edx
 	push  ecx
 	push  ebx
-  
-  mov eax,  0x02000000       ; in 8.24
-  mul eax      ; in * in = edx eax
-  mov ebx,  eax ; ebx = in ^ 2
 
-  mov esi,  0x02000000  ; 除数 8.24
-  div esi      ; edx eax / esi = eax
-  pop esi
+  mov esi,  0x01000000
+  
+  ; x^2/2!
+  mul eax      ; in * in = edx eax
+  mov ebx,  0x02000000  ; 除数 8.24
+  div ebx      ; edx eax / ebx = eax
+  sub esi,  eax
+  push  eax
+  
+  mul eax     ; (in ^ 2 / 2!) ^ 2
+  mov ebx,  0x06000000  ; in ^ 4 / 4! 
+  div ebx     ; edx eax / ebx = eax
+  add esi,  eax
+  
+  mov ecx,  eax ; ecx = in ^ 4 / 4! 
+  pop eax       ; eax = in ^ 2 / 2!
+
+  mul ecx       ; eax * ecx = edx eax
+  mov edi,  0x0f000000 ; edx = 15
+  div edi       ; edx eax / edi = eax
+  sub esi,  eax
+
+
 endp:
-  call  print_eax_frac
+  mov eax,  esi
   pop ebx
   pop ecx
   pop edi
--- chap12/test.s
+++ chap12/test.s
@@ -3,7 +3,9 @@
   extern  cos, print_eax_frac
 
 _start:
+  mov eax,  0x02000000
   call cos
+  call print_eax_frac
   mov eax,  1
   mov ebx,  0
   int 0x80
