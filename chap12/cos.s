b44ed6ba82dfd3b5d27d8e5fdd1cf51aea3a76b9
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-11-21 14:36:56 +0000

    supported over pi/4. executed sccesfully

--- chap12/cos.s
+++ chap12/cos.s
@@ -21,11 +21,13 @@ pr:
   mov ecx,  pi
   cmp eax,  ebx
   jle pr1
+  mov edi,  1
+  push  edi
   sub ecx,  eax   ; pi - x
   mov eax,  ecx   ; eax = pi - x :: in'
 pr1:
   ;; in' / 2
-  shr eax,  2   ; eax / 2 :: in' / 2
+  shr eax,  1   ; eax / 2 :: in' / 2
 
 cos_eax:
   ; x^2/2!
@@ -56,8 +58,13 @@ cos_eax:
   ;; edx eax (16.48 -> 8.24)    ; 2 * cos(in/2)
   mov ebx,  0x00800000  ; 0.5 [8.24]
   div ebx
-  mov ebx,  0xffffffff  ; -1 [8.24]
-  add eax,  ebx
+  mov ebx,  0x01000000  ; 1 [8.24]
+  sub eax,  ebx
+  pop edi
+  cmp edi,  1
+  jne endp
+  not eax
+  inc eax
 endp:
   pop ebx
   pop ecx
