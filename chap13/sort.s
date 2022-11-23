1486a50ea2705396c7acac18f4c7aafd03071a1e
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2022-11-23 05:55:58 +0000

    coding without swap.[Error : segmentation fault]

--- chap13/sort.s
+++ chap13/sort.s
@@ -7,22 +7,36 @@ sort:
   push  ecx
   push  ebx
   push  eax
+  
+  ;; ---入力データを整理---
+  mov ebx,  [esp + 4] ; data
+  mov ecx,  [esp + 8] ; length
+  mov eax,  [esp + 12]; sot       ; width
+  mov esi,  4
+  mov edx,  0
+  div esi   ; edx eax / esi = eax
+  mov esi,  eax
+  ;; ---
+  ;; ebx  : 先頭番地
+  ;; ecx  : データ個数
+  ;; esi  : 構造体データ個数
 
   mov   eax,  0 ; max_index
   mov   edx,  0 ; max
-  dec   ecx
+  sub   ecx,  esi   ; i = length - sot
 loop0:
   cmp   ecx,  0 ; ecx = i
   jle   endp
   mov   edx,  [ebx]   ; max = data[0]
   mov   eax,  0       ; max_indent = 0
 
-  mov   edi,  1 ; edi = j
+  mov   edi,  esi ; edi = sot :: j
 
   loop1:
     cmp   edi,  ecx   ; j > i?
-    jg    loop0l
-
+    jg    swap
+    
+    push  esi
     mov   esi,  [ebx + edi*4] ; data[j]
     cmp   esi,  edx       ; data[j] >= max
     jge   then
@@ -32,10 +46,11 @@ loop0:
       mov edx,  [ebx + edi*4] ; max = data[j]
       mov eax,  edi           ; max_index = j
     endif:
+      pop esi
       inc edi
       jmp loop1
 
-  loop0l:
+  swap:
     push esi
     push edi
     mov   esi,  [ebx + eax*4]   ; m = data[max_index]
